6f30bd172d697e8a4000f3fac761c437edc04963
LiRezende <librusamolin@gmail.com>
2023-10-23 16:53:04 -0300

    Project configuration.

---
+++ src/main/java/br/edu/infnet/apppaws/apppaws/AppPawsApplication.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.apppaws.apppaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppPawsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppPawsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/infnet/apppaws/apppaws/AppPawsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.apppaws.apppaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppPawsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
