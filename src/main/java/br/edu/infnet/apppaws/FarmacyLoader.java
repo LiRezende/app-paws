d44357b10492f673b9752e73db7a718f1672c27e
LiRezende <librusamolin@gmail.com>
2023-10-29 21:05:56 -0300

    Feature 02: correção da renderização da página jsp.

--- src/main/java/br/edu/infnet/apppaws/FarmacyLoader.java
+++ src/main/java/br/edu/infnet/apppaws/FarmacyLoader.java
@@ -1,57 +1,57 @@
-package br.edu.infnet.apppaws;
-
-import br.edu.infnet.apppaws.models.domains.Farmacy;
-import br.edu.infnet.apppaws.models.services.FarmacyService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
-import org.springframework.core.annotation.Order;
-import org.springframework.stereotype.Component;
-
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.util.UUID;
-
-@Order(4)
-@Component
-public class FarmacyLoader implements ApplicationRunner {
-
-    @Autowired
-    private FarmacyService farmacyService;
-
-    @Override
-    public void run(ApplicationArguments args) throws Exception {
-
-        FileReader file = new FileReader("files/farmacy.txt");
-        BufferedReader reader = new BufferedReader(file);
-
-        String line = reader.readLine();
-
-        String[] attribute = null;
-
-        while(line != null) {
-
-            attribute = line.split(";");
-
-            Farmacy farmacy = new Farmacy();
-
-            farmacy.setCodId(UUID.fromString(attribute[0]));
-            farmacy.setName(attribute[1]);
-            farmacy.setDescription(attribute[2]);
-            farmacy.setPrice(Float.valueOf(attribute[3]));
-            farmacy.setInventory(Boolean.valueOf(attribute[4]));
-            farmacy.setDosage(attribute[5]);
-            farmacy.setLaboratory(attribute[6]);
-
-            farmacyService.include(farmacy);
-
-            line = reader.readLine();
-        }
-
-        for(Farmacy farmacy: farmacyService.getAllList()) {
-            System.out.println("[Farmacy] " + farmacy);
-        }
-
-        reader.close();
-    }
-}
+//package br.edu.infnet.apppaws;
+//
+//import br.edu.infnet.apppaws.models.domains.Farmacy;
+//import br.edu.infnet.apppaws.models.services.FarmacyService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.ApplicationArguments;
+//import org.springframework.boot.ApplicationRunner;
+//import org.springframework.core.annotation.Order;
+//import org.springframework.stereotype.Component;
+//
+//import java.io.BufferedReader;
+//import java.io.FileReader;
+//import java.util.UUID;
+//
+//@Order(4)
+//@Component
+//public class FarmacyLoader implements ApplicationRunner {
+//
+//    @Autowired
+//    private FarmacyService farmacyService;
+//
+//    @Override
+//    public void run(ApplicationArguments args) throws Exception {
+//
+//        FileReader file = new FileReader("files/farmacy.txt");
+//        BufferedReader reader = new BufferedReader(file);
+//
+//        String line = reader.readLine();
+//
+//        String[] attribute = null;
+//
+//        while(line != null) {
+//
+//            attribute = line.split(";");
+//
+//            Farmacy farmacy = new Farmacy();
+//
+//            farmacy.setCodId(UUID.fromString(attribute[0]));
+//            farmacy.setName(attribute[1]);
+//            farmacy.setDescription(attribute[2]);
+//            farmacy.setPrice(Float.valueOf(attribute[3]));
+//            farmacy.setInventory(Boolean.valueOf(attribute[4]));
+//            farmacy.setDosage(attribute[5]);
+//            farmacy.setLaboratory(attribute[6]);
+//
+//            farmacyService.include(farmacy);
+//
+//            line = reader.readLine();
+//        }
+//
+//        for(Farmacy farmacy: farmacyService.getAllList()) {
+//            System.out.println("[Farmacy] " + farmacy);
+//        }
+//
+//        reader.close();
+//    }
+//}
--- src/main/java/br/edu/infnet/apppaws/ToyLoader.java
+++ src/main/java/br/edu/infnet/apppaws/ToyLoader.java
@@ -1,57 +1,57 @@
-package br.edu.infnet.apppaws;
-
-import br.edu.infnet.apppaws.models.domains.Toy;
-import br.edu.infnet.apppaws.models.services.ToyService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
-import org.springframework.core.annotation.Order;
-import org.springframework.stereotype.Component;
-
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.util.UUID;
-
-@Order(3)
-@Component
-public class ToyLoader implements ApplicationRunner {
-
-    @Autowired
-    private ToyService toyService;
-
-    @Override
-    public void run(ApplicationArguments args) throws Exception {
-
-        FileReader file = new FileReader("files/toy.txt");
-        BufferedReader reader = new BufferedReader(file);
-
-        String line = reader.readLine();
-
-        String[] attribute = null;
-
-        while(line != null) {
-
-            attribute = line.split(";");
-
-            Toy toy = new Toy();
-
-            toy.setCodId(UUID.fromString(attribute[0]));
-            toy.setName(attribute[1]);
-            toy.setDescription(attribute[2]);
-            toy.setPrice(Float.valueOf(attribute[3]));
-            toy.setInventory(Boolean.valueOf(attribute[4]));
-            toy.setForPuppies(Boolean.valueOf(attribute[5]));
-            toy.setMaterial(attribute[6]);
-
-            toyService.include(toy);
-
-            line = reader.readLine();
-        }
-
-        for(Toy toy: toyService.getAllList()) {
-            System.out.println("[Toy] " + toy);
-        }
-
-        reader.close();
-    }
-}
+//package br.edu.infnet.apppaws;
+//
+//import br.edu.infnet.apppaws.models.domains.Toy;
+//import br.edu.infnet.apppaws.models.services.ToyService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.ApplicationArguments;
+//import org.springframework.boot.ApplicationRunner;
+//import org.springframework.core.annotation.Order;
+//import org.springframework.stereotype.Component;
+//
+//import java.io.BufferedReader;
+//import java.io.FileReader;
+//import java.util.UUID;
+//
+//@Order(3)
+//@Component
+//public class ToyLoader implements ApplicationRunner {
+//
+//    @Autowired
+//    private ToyService toyService;
+//
+//    @Override
+//    public void run(ApplicationArguments args) throws Exception {
+//
+//        FileReader file = new FileReader("files/toy.txt");
+//        BufferedReader reader = new BufferedReader(file);
+//
+//        String line = reader.readLine();
+//
+//        String[] attribute = null;
+//
+//        while(line != null) {
+//
+//            attribute = line.split(";");
+//
+//            Toy toy = new Toy();
+//
+//            toy.setCodId(UUID.fromString(attribute[0]));
+//            toy.setName(attribute[1]);
+//            toy.setDescription(attribute[2]);
+//            toy.setPrice(Float.valueOf(attribute[3]));
+//            toy.setInventory(Boolean.valueOf(attribute[4]));
+//            toy.setForPuppies(Boolean.valueOf(attribute[5]));
+//            toy.setMaterial(attribute[6]);
+//
+//            toyService.include(toy);
+//
+//            line = reader.readLine();
+//        }
+//
+//        for(Toy toy: toyService.getAllList()) {
+//            System.out.println("[Toy] " + toy);
+//        }
+//
+//        reader.close();
+//    }
+//}
